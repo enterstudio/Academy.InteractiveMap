7c8b528bdfa4f88e7d8cd269ba49084c2e4209d2
andrius-diu <andrius.diud@gmail.com>
2013-07-11 12:58:32 +0300

    test solution for deployment testing

---
+++ MvcTestSolution/MvcGeoMap/Controllers/D3Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcGeoMap.Controllers
+{
+    public class D3Controller : Controller
+    {
+        //
+        // GET: /D3/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
+
+
---
+++ MvcTestSolution/MvcGeoMap/Scripts/d3tests/constants.js
@@ -0,0 +1,6 @@
+﻿var width = 1000,
+    height = 600,
+    centered;
+var projection = d3.geo.equirectangular()
+    .scale(150);
+var path = d3.geo.path().projection(projection);
