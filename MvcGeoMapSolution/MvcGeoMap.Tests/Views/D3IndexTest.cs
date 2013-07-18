3855353d2241b28162d4feae56e6ea6a98b5f140
ringaile <ringaile@gmail.com>
2013-07-18 13:26:12 +0300

    test

---
+++ MvcGeoMapSolution/MvcGeoMap.Tests/Views/D3IndexTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcGeoMap.Tests.Views
+{
+    [TestClass]
+    public class D3IndexTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var projection = d3.geo.mercator();
+        }
+    }
+}
